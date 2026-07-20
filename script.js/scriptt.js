1b88934644b684139c0812f3ad8bb66b2c163de0
Dev-Miguellima <dev.miguellima@gmail.com>
2026-07-20 18:51:57 -0300

    projeto sistema de agendamento

---
+++ script.js/scriptt.js
@@ -0,0 +1,31 @@
+function agendar() {
+  const nome = document.getElementById("nome").value.trim();
+
+  const data = document.getElementById("data").value;
+
+  const hora = document.getElementById("hora").value;
+
+  const somenteLetras = /^[A-Za-zÀ-ÿ\s]+$/;
+
+  if (!nome || !data || !hora) {
+    alert("Preencha todos os campos!");
+    return;
+  }
+
+  if (!somenteLetras.test(nome)) {
+    alert("Digite apenas letras no campo nome!");
+    return;
+  }
+
+  const lista = document.getElementById("listaAgendamentos");
+
+  const item = document.createElement("p");
+
+  item.textContent = `${nome} agendou para ${data} às ${hora}`;
+
+  lista.appendChild(item);
+
+  document.getElementById("nome").value = "";
+  document.getElementById("data").value = "";
+  document.getElementById("hora").value = "";
+}
